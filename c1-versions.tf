c842075b2094c1d1d076e581d5e4c25d3a316a65
Josh A <info@josepharturo.com>
2022-08-09 11:48:48 +0200

    c1-version_updgrade
    c2-resource-group_upgrade

--- c1-versions.tf
+++ c1-versions.tf
@@ -1,14 +1,18 @@
-# Terraform Block
+
 terraform {
-    required_version = ">= 1.0.0"
-    required_providers {
-        azurerm = {
-            source = "hashicorp/azurerm"
-            version = ">=3.17.0"
-        }
+  required_providers {
+    
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
     }
+  }
 }
 
 provider "azurerm" {
-    features {}
+  features {}
+
+  subscription_id = "500cebd9-6205-4ba8-8b1d-94f3c8b32d32"
+  tenant_id       = "c66b6765-b794-4a2b-84ed-845b341c086a"
 }
+
---
+++ c2-resource-group.tf
@@ -0,0 +1,5 @@
+
+resource "azurerm_resource_group" "test" {
+    name = "myrg-1"
+    location = "West Europe"
+}
