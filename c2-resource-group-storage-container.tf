b74e0a18c061c55117d3640926a370fb8eded259
Josh A <info@josepharturo.com>
2022-08-09 16:29:50 +0200

    UPDATE c2-resource formatting

--- c2-resource-group-storage-container.tf
+++ c2-resource-group-storage-container.tf
@@ -1,7 +1,7 @@
 
 resource "azurerm_resource_group" "terraform_test_resource" {
-    name = "terraform_test_resource-1"
-    location = "West Europe"
+    name            = "terraform_test_resource-1"
+    location        = "West Europe"
 
     tags = {
         environment = "staging"
@@ -11,19 +11,19 @@ resource "azurerm_resource_group" "terraform_test_resource" {
 # Resource 2: Random string
 
 resource "random_string" "testrandom" {
-    length = 7
+    length  = 7
     special = false
-    upper = false
+    upper   = false
 }
 
 # Resource 3: Azure Storage Account
 
 resource "azurerm_storage_account" "terraform_test_sa" {
     name = "terraformtestsa${random_string.testrandom.id}"
-    resource_group_name = azurerm_resource_group.terraform_test_resource.name
-    location = azurerm_resource_group.terraform_test_resource.location
-    account_tier = "Standard"
-    account_replication_type = "GRS"
+    resource_group_name         = azurerm_resource_group.terraform_test_resource.name
+    location                    = azurerm_resource_group.terraform_test_resource.location
+    account_tier                = "Standard"
+    account_replication_type    = "GRS"
 
     tags = {
         environment = "staging"
@@ -31,7 +31,7 @@ resource "azurerm_storage_account" "terraform_test_sa" {
 }
 
 resource "azurerm_storage_encryption_scope" "terraform_test_sae_scope" {
-    name = "microsoftmanaged"
-    storage_account_id = azurerm_storage_account.terraform_test_sa.id
-    source = "Microsoft.Storage"
+    name                        = "microsoftmanaged"
+    storage_account_id          = azurerm_storage_account.terraform_test_sa.id
+    source                      = "Microsoft.Storage"
 }
