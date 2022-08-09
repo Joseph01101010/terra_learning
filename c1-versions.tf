753b95cf199ad80068410a292d35de666dbcf26f
Josh A <info@josepharturo.com>
2022-08-09 12:23:13 +0200

    ADD multiple providers/resources

--- c1-versions.tf
+++ c1-versions.tf
@@ -15,4 +15,3 @@ provider "azurerm" {
   subscription_id = "500cebd9-6205-4ba8-8b1d-94f3c8b32d32"
   tenant_id       = "c66b6765-b794-4a2b-84ed-845b341c086a"
 }
-
--- c2-resource-group.tf
+++ c2-resource-group.tf
@@ -2,4 +2,36 @@
 resource "azurerm_resource_group" "terraform_test_resource" {
     name = "terraform_test_resource-1"
     location = "West Europe"
+
+    tags = {
+        environment = "staging"
+    }
+}
+
+# Resource 2: Random string
+
+resource "random_string" "testrandom" {
+    length = 7
+    special = false
+    upper = false
+}
+
+# Resource 3: Azure Storage Account
+
+resource "azurerm_storage_account" "terraform_test_sa" {
+    name = "terraformtestsa${random_string.testrandom.id}"
+    resource_group_name = azurerm_resource_group.terraform_test_resource.name
+    location = azurerm_resource_group.terraform_test_resource.location
+    account_tier = "Standard"
+    account_replication_type = "GRS"
+
+    tags = {
+        environment = "staging"
+    }
+}
+
+resource "azurerm_storage_encryption_scope" "terraform_test_sae_scope" {
+    name = "microsoftmanaged"
+    storage_account_id = azurerm_storage_account.terraform_test_sa.id
+    source = "Microsoft.Storage"
 }
